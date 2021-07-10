61ee15c5d779b5524f10204e2c103c7bc5b07123
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-10 15:28:10 +0300

    Add urls.py to insta app

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http  import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+# from .forms import SignupForm, ImageForm, ProfileForm, CommentForm
+# from .emails import send_activation_email
+# from .tokens import account_activation_token
+# from .models import Image, Profile, Comments
 
-# Create your views here.
+
+def signup(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                to_email = form.cleaned_data.get('email')
+                return HttpResponse('Confirm your email address to complete registration')
+        else:
+            form = SignupForm()
+            return render(request, 'registration/signup.html',{'form':form})
+
