52452ad2342697a2ac2a024a9547939a48fd0c4f
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-12 12:39:05 +0300

    Add login redirect to settings

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,5 @@
-from .forms import SignupForm
+
+from insta.forms import SignUpForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
@@ -13,22 +14,21 @@ def home(request):
     return render(request, 'index.html')
 
 def signup(request):
-    if request.user.is_authenticated():
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            form = SignupForm(request.POST)
-            if form.is_valid():
-                user = form.save(commit=False)
-                user.is_active = False
-                user.save()
-                current_site = get_current_site(request)
-                to_email = form.cleaned_data.get('email')
-                send_activation_email(user, current_site, to_email)
-                return HttpResponse('Confirm your email address to complete registration')
-        else:
-            form = SignupForm()
-            return render(request, 'registration/signup.html',{'form':form})
+    # if request.user.is_authenticated():
+    #     return redirect('home')
+    # else:
+    #     if request.method == 'POST':
+    #         form = SignupForm(request.POST)
+    #         if form.is_valid():
+    #             user = form.save(commit=False)
+    #             user.is_active = False
+    #             user.save()
+    #             current_site = get_current_site(request)
+    #             to_email = form.cleaned_data.get('email')
+    #             send_activation_email(user, current_site, to_email)
+    #             return HttpResponse('Confirm your email address to complete registration')
+        form = SignUpForm()
+        return render(request, 'registration/signup.html',{'form':form})
 
 
 
