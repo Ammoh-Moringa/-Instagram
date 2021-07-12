d33a11a35bab11c50a89713704d7e2579231856a
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-12 14:01:41 +0300

    Add profile templates

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,5 @@
 from insta.forms import SignUpForm
+from insta.models import Image
 from django.shortcuts import render,redirect
 
 # Create your views here.
@@ -13,43 +14,45 @@ from django.contrib.auth.decorators import login_required
 from .emails import send_activation_email
 from .tokens import account_activation_token
 
-@login_required(login_url='/')
 def home(request):
-    
+    images = Image.get_all_images()
     
     return render(request, 'index.html', {'images':images})
-
 def signup(request):
-        if request.method == 'POST':
-            form = SignUpForm(request.POST)
-            if form.is_valid():
-                user = form.save(commit=False)
-                user.is_active = False
-                user.save()
-                current_site = get_current_site(request)
-                to_email = form.cleaned_data.get('email')
-                send_activation_email(user, current_site, to_email)
-        return render(request, 'registration/signup.html',{'form':form})
-        # else:
-        #     form = SignupForm()
-        #     return render(request, 'registration/signup.html',{'form':form})
+    form = SignUpForm()
+    return render(request, 'registration/signup.html',{'form':form})
+    # if request.user.is_authenticated():
+    #     return redirect('home')
+    # else:
+    #     if request.method == 'POST':
+    #         form = SignupForm(request.POST)
+    #         if form.is_valid():
+    #             user = form.save(commit=False)
+    #             user.is_active = False
+    #             user.save()
+    #             current_site = get_current_site(request)
+    #             to_email = form.cleaned_data.get('email')
+    #             send_activation_email(user, current_site, to_email)
+    #             return HttpResponse('Confirm your email address to complete registration')
+    # if:
+    #     form = SignUpForm()
+    #     return render(request, 'home',{'form':form})
+# def activate(request, uidb64, token):
+#     try:
+#         uid = force_text(urlsafe_base64_decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#         user = None
 
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        # form = LoginForm()
-        # return redirect('home')
-        return HttpResponse('registration/login.html')
-    else:
-        return HttpResponse('Activation link is invalid')
+#     if user is not None and account_activation_token.check_token(user, token):
+#         user.is_active = True
+#         user.save()
+#         login(request, user)
+#         # form = LoginForm()
+#         # return redirect('home')
+#         return HttpResponse('registration/login.html')
+#     else:
+#         return HttpResponse('Activation link is invalid')
     
 # def profile(request, username):
 #     profile = User.objects.get(username=username)
