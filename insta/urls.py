275a20c740a39e7f17cd3e4ba056fde8faf30d20
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-13 14:31:43 +0300

    Add view url

--- insta/urls.py
+++ insta/urls.py
@@ -14,6 +14,7 @@ urlpatterns=[
     url(r'^upload/$', views.upload_image, name='upload_image'),
     url(r'^accounts/edit/',views.edit_profile, name='edit_profile'),
     url(r'^image/(?P<image_id>\d+)', views.single_image, name='single_image'),
+    url(r'^new/image$', views.new_image, name='new-image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
